64e2b24097500852d0c225fbd6fcdf4111fd1d7a
afpa <remy.deprez.59790@gmail.com>
2021-05-25 17:28:31 +0200

    ajout du controller navigation

--- src/main/java/fr/formation/afpa/controller/NavigationController.java
+++ src/main/java/fr/formation/afpa/controller/NavigationController.java
@@ -7,15 +7,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class NavigationController {
 	
-	
+//	Methode de redirection à l'acceuil par défaut
 	@RequestMapping(value = "/")
 	public String index(Model model) {
 		return "index";
 	}
-	
+//	Methode qui est lancée pour l'obtention du formulaire d'inscription
 	@RequestMapping(value = "/getform")
 	public String getForm(Model model) {
 		return "inscription";
+	}
+//	Methode qui est lancée pour l'obtention de la page de gestion de la colocation
+	@RequestMapping(value = "/getgestion")
+	public String getGestion(Model model) {
+		return "gestionColoc";
 	}
 
 }
