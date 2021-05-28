63578c32b739306f55b98cebfd92ace56d00cea3
Rémy <remy.deprez.59790@gmail.com>
2021-05-28 11:55:04 +0200

    modification formulaire
    Modification du formaulaire d'insription, ajout de methodes dans le controller pour le crud des comptes, ajout de la page modif profile avec le boutton delete profile et la methode associé dans le controller

--- src/main/java/fr/formation/afpa/domain/Status.java
+++ src/main/java/fr/formation/afpa/domain/Status.java
@@ -1,5 +1,9 @@
 package fr.formation.afpa.domain;
 
+
+//Enum servant à la selection du status de l'utilisateur a l'inscription, en d'autres termes, ce sont les options du radio bouton
+
+
 public enum Status {
 	
 	Colocataire,
