d7f0ca6dff2dce8481b717ad8c7bce537cf01ab8
Rémy <remy.deprez.59790@gmail.com>
2021-05-30 13:24:33 +0200

    Ajout de l'encryptage du mdp
    Ajout de l'encryptage du mdp en bdd qui permet également de se connecter avec un compte créer par un utilisateur

--- src/main/java/fr/formation/afpa/controller/InscriptionController.java
+++ src/main/java/fr/formation/afpa/controller/InscriptionController.java
@@ -2,40 +2,61 @@ package fr.formation.afpa.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import fr.formation.afpa.domain.AppUser;
+import fr.formation.afpa.repository.UserRepository;
 import fr.formation.afpa.service.IUtilisateurService;
 
 @Controller
 public class InscriptionController {
-	
+
 	@Autowired
-	IUtilisateurService service ;
-	
+	UserRepository userRepo;
+	@Autowired
+	IUtilisateurService service;
+
+	// Encryte Password with BCryptPasswordEncoder
+	public static String encrytePassword(String password) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder.encode(password);
+	}
+
 //	Methode lancée lorsque le formulaire est envoyé
-	
+
 	@RequestMapping(value = "/createaccount")
-	public String index(Model model,AppUser appuser) {
-		model.addAttribute("appuser", appuser );
+	public String index(Model model, AppUser appuser) {
+		String encrytedPassword = encrytePassword(appuser.getEncrytedPassword());
+		appuser.setEncrytedPassword(encrytedPassword);
+		model.addAttribute("appuser", appuser);
 		service.saveOrUpdate(appuser);
 		return "index";
 	}
-	
+
+//methode lancée lorsque l'on demande le formulaire d'update de profil
+	@RequestMapping(value = "/getprofile")
+	public String getProfile(Model model, Authentication auth, AppUser appuser) {
+		System.out.println(userRepo.findByUserName(auth.getName()));
+		model.addAttribute("appuser", userRepo.findByUserName(auth.getName()));
+
+		return "modifprofile";
+	}
+
 //methpode lancée lorsque l'on appuie sur le boutton "valider" de l'update
 	@RequestMapping(value = "/updateaccount")
-	public String updateAccount(Model model,AppUser appuser) {
-		model.addAttribute("appuser", appuser );
+	public String updateAccount(Model model, AppUser appuser) {
+		model.addAttribute("appuser", appuser);
 		service.saveOrUpdate(appuser);
 		return "index";
 	}
 //methode lancée lorsque l'on appuie sur le button delete de l'update
-	
+
 	@RequestMapping(value = "/deleteaccount")
-	public String deleteAccount(Model model,AppUser appuser) {
-		model.addAttribute("appuser", appuser );
+	public String deleteAccount(Model model, AppUser appuser) {
+		model.addAttribute("appuser", appuser);
 		service.delete(appuser);
 		return "index";
 	}
