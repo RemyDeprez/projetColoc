e553d7d7c236336777a50888bded5c2f52c8c200
memaerten <me.maerten@gmail.com>
2021-05-31 09:43:20 +0200

    Pages nécessitant une connexion renvoient vers connexion lorsque compte non connecté

--- src/main/java/fr/formation/afpa/controller/NavigationController.java
+++ src/main/java/fr/formation/afpa/controller/NavigationController.java
@@ -10,7 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import fr.formation.afpa.domain.Location;
+import fr.formation.afpa.domain.AppUser;
+import fr.formation.afpa.domain.Utilisateur;
 import fr.formation.afpa.service.LocationService;
 import fr.formation.afpa.utils.WebUtils;
 
@@ -19,6 +20,8 @@ public class NavigationController {
 
 	@Autowired
 	LocationService service;
+	
+	
 
 	//	********************** NAVIGATION GENERALE ********************************************
 
@@ -44,9 +47,9 @@ public class NavigationController {
 		return "index";
 	}
 	//	Methode qui est lancée pour l'obtention du formulaire d'inscription
-	@RequestMapping(value = "/getform")
+	@RequestMapping(value = "/inscription")
 	public String getForm(Model model) {
-		Utilisateur utilisateur = new Utilisateur();
+		AppUser utilisateur = new AppUser();
 		model.addAttribute("utilisateur", utilisateur);
 		return "inscription";
 	}
@@ -70,27 +73,70 @@ public class NavigationController {
 
 
 	@RequestMapping(path = "/connexion", method  = RequestMethod.GET)
-	public String getConnexion() {
+	public String getConnexion(Model model, Principal principal) {
+		if(principal != null) {
+			return "redirect:/index";
+		}
 		return "connexion";
 	}
 
 	@RequestMapping(path = "/ajout", method  = RequestMethod.GET)
-	public String getAjout() {
+	public String getAjout(Model model, Principal principal) {
+		if(principal != null) {
+			String userName = principal.getName();
+
+			System.out.println("User Name: " + userName);
+
+			User loginedUser = (User) ((Authentication) principal).getPrincipal();
+
+			String userInfo = WebUtils.toString(loginedUser);
+			model.addAttribute("userInfo", userInfo);
+		}
 		return "ajout";
 	}
 
 	@RequestMapping(path = "/modif", method  = RequestMethod.GET)
-	public String getModif() {
+	public String getModif(Model model, Principal principal) {
+		if(principal != null) {
+			String userName = principal.getName();
+
+			System.out.println("User Name: " + userName);
+
+			User loginedUser = (User) ((Authentication) principal).getPrincipal();
+
+			String userInfo = WebUtils.toString(loginedUser);
+			model.addAttribute("userInfo", userInfo);
+		}
 		return "modif";
 	}
 
 	@RequestMapping(path = "/messagerie", method  = RequestMethod.GET)
-	public String getMessagerie() {
+	public String getMessagerie(Model model, Principal principal) {
+		if(principal != null) {
+			String userName = principal.getName();
+
+			System.out.println("User Name: " + userName);
+
+			User loginedUser = (User) ((Authentication) principal).getPrincipal();
+
+			String userInfo = WebUtils.toString(loginedUser);
+			model.addAttribute("userInfo", userInfo);
+		}
 		return "messagerie";
 	}
 
 	@RequestMapping(path = "/rechercheLocation", method  = RequestMethod.GET)
-	public String getRechercheLocation() {
+	public String getRechercheLocation(Model model, Principal principal) {
+		if(principal != null) {
+			String userName = principal.getName();
+
+			System.out.println("User Name: " + userName);
+
+			User loginedUser = (User) ((Authentication) principal).getPrincipal();
+
+			String userInfo = WebUtils.toString(loginedUser);
+			model.addAttribute("userInfo", userInfo);
+		}
 		return "rechercheLocation";
 	}
 
