d4d1ae8917d60374fda7aa0385b2820635984881
Rémy <remy.deprez.59790@gmail.com>
2021-06-04 16:54:53 +0200

    Ajout de google map
    Ajout de /getMap

--- src/main/java/fr/formation/afpa/controller/NavigationController.java
+++ src/main/java/fr/formation/afpa/controller/NavigationController.java
@@ -190,6 +190,13 @@ public class NavigationController {
 		return "rechercheLocation";
 	}
 	
+	//methode de redirection pour les tests de google map
+	@RequestMapping(value = "/getMap")
+	public String getMap(Model model) {
+		model.addAttribute("listloc" , service.findAll());
+		return "maptest";
+	}
+	
 
 
 
